0457db754c4dc77d86d4053b60fc7572eff60043
VaideshWaranR <vaiv64259@gmail.com>
2025-07-28 17:19:52 +0000

    Done some server code

---
+++ src/main.c
@@ -0,0 +1,5 @@
+#include "../include/server.h"
+int main(){
+	start_server();
+	return 0;
+}
---
+++ src/server.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <pthread.h>
+//#include "command_handler.h" 
+#include "../include/server.h"
+#define PORT 6379
+#define BUFFER_SIZE 1024
+
+
+void* handle_client(void* arg) {
+    int client_socket = *(int*)arg;
+    free(arg);
+
+    char buffer[BUFFER_SIZE];
+
+    while (1) {
+        memset(buffer, 0, BUFFER_SIZE);
+        int bytes_read = read(client_socket, buffer, BUFFER_SIZE - 1);
+        if (bytes_read <= 0) break;
+        fwrite(buffer,1,bytes_read,stdout);
+        //char* response = handle_command(buffer); 
+        //write(client_socket, response, strlen(response));
+        //free(response);  
+    }
+
+    close(client_socket);
+    pthread_exit(NULL);
+}
+
+void start_server() {
+    int server_fd, *client_socket;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == 0) {
+        perror("Socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 10) < 0) {
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server running on port %d...\n", PORT);
+
+    while (1) {
+        client_socket = malloc(sizeof(int));
+        *client_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+        if (*client_socket < 0) {
+            perror("Accept failed");
+            continue;
+        }
+
+        pthread_t thread_id;
+        pthread_create(&thread_id, NULL, handle_client, client_socket);
+        pthread_detach(thread_id);  
+    }
+
+    close(server_fd);
+}
+
